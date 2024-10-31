bd94f4eb79dd1cf08bf98fc0e723df705fabbc3c
Álvaro Serrano <alvaroserranoe@gmail.com>
2024-10-31 10:31:16 +0100

    Añadida función ex log()

--- app/src/main/java/com/ase/pokedex/common/ex/Ex.kt
+++ app/src/main/java/com/ase/pokedex/common/ex/Ex.kt
@@ -2,6 +2,7 @@ package com.ase.pokedex.common.ex
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.util.Log
 import android.widget.Toast
 import java.security.MessageDigest
 import kotlin.text.toByteArray
@@ -9,4 +10,8 @@ import kotlin.text.toHexString
 
 fun String.toast(context: Context) {
     Toast.makeText(context, this, Toast.LENGTH_SHORT).show()
+}
+
+fun String.log() {
+    Log.d("Pokédex", this)
 }
