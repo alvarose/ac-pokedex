e3bcbb1e32f7c677986ef7869489636ef3973ede
Álvaro Serrano <alvaroserranoe@gmail.com>
2024-11-29 19:24:36 +0100

    Migrado a StateFlow & Result

---
+++ app/src/main/java/com/ase/pokedex/data/datasource/remote/ApiClient.kt
@@ -0,0 +1,30 @@
+package com.ase.pokedex.data.datasource.remote
+
+import com.ase.pokedex.BuildConfig
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.create
+
+object ApiClient {
+
+    val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    val instance = Retrofit.Builder()
+        .baseUrl(BuildConfig.API_URL)
+        .client(okHttpClient)
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .build()
+        .create<PokemonService>()
+}
